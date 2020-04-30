6e57196fafc8047e73cd7686c7bbb59f7a608d06
amueller <andreas.mueller@bcmsolutions.de>
2020-04-30 11:12:21 +0200

    Initial commit
    Initial commit after setup.

---
+++ PetAndOwnerService/src/main/java/am/training/petclinic/PetAndOwnerService/PetAndOwnerServiceApplication.java
@@ -0,0 +1,13 @@
+package am.training.petclinic.PetAndOwnerService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetAndOwnerServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PetAndOwnerServiceApplication.class, args);
+	}
+
+}
---
+++ PetAndOwnerService/src/test/java/am/training/petclinic/PetAndOwnerService/PetAndOwnerServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package am.training.petclinic.PetAndOwnerService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetAndOwnerServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
